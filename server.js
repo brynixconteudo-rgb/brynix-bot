898e646c5ec0c655556de29cf2727891840d6fea
brynixconteudo-rgb <brynixconteudo@gmail.com>
2025-09-21 02:57:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { initWhatsApp } = require('./whatsapp');
+const { initWhatsApp } = require('./whatsapp'); // IMPORTA O MÓDULO
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Rota de teste
 app.get('/', (_req, res) => res.send('BRYNIX WhatsApp Bot up ✅'));
 
+// REGISTRA AS ROTAS DO WHATSAPP
 initWhatsApp(app);
 
 const port = process.env.PORT || 3000;
